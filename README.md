ea8c75e1355dc78eb5a73d473e5ff98a40dca4b0
officeyutong <yt.xyxx@gmail.com>
2021-02-18 21:41:59 +0800

    add a little bit of feature

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# testrepo
+# Hello
--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import asyncio
 
 import config
 import aiofiles
+import ujson
 HEADERS = {
     "Accept": "application/vnd.github.v3+json",
     "Authorization": f"token {config.OAUTH_TOKEN}"
 }
 
-async def fetch_stars(client: aiohttp.ClientSession):
+
+async def fetch_data(client: aiohttp.ClientSession):
     resp = await client.get(f"https://api.github.com/repos/Officeyutong/testrepo")
     json_resp = await resp.json()
-    return json_resp["stargazers_count"]
+    async with aiofiles.open("output.json", "w", encoding="utf-8") as f:
+        await f.write(ujson.dumps(json_resp))
+    return json_resp["stargazers_count"], json_resp["open_issues_count"]
 
 
 async def update_repo_name(client: aiohttp.ClientSession, name: str):
@@ -24,9 +28,9 @@ async def main():
     async with aiohttp.ClientSession(headers=HEADERS) as client:
         client: aiohttp.ClientSession
         while True:
-            stars = await fetch_stars(client)
-            print(stars)
-            resp = await update_repo_name(client, f"This-Repo-Has-Less-Than-{stars+1}-Stars")
+            stars, issues = await fetch_data(client)
+            print(stars, issues)
+            resp = await update_repo_name(client, f"This-Repo-Has-{issues}-Issues-And-Less-Than-{stars+1}-Stars")
             print(resp)
             await asyncio.sleep(3)
 asyncio.get_event_loop().run_until_complete(main())
