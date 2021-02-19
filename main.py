5eaf8b52829443a21182b6c47fd16f02125c2761
officeyutong <yt.xyxx@gmail.com>
2021-02-19 13:13:02 +0800

    exception handle

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import aiohttp
 import asyncio
-
 import config
-import aiofiles
-import ujson
 HEADERS = {
     "Accept": "application/vnd.github.v3+json",
     "Authorization": f"token {config.OAUTH_TOKEN}"
@@ -13,8 +10,6 @@ HEADERS = {
 async def fetch_data(client: aiohttp.ClientSession):
     resp = await client.get(f"https://api.github.com/repos/Officeyutong/testrepo")
     json_resp = await resp.json()
-    async with aiofiles.open("output.json", "w", encoding="utf-8") as f:
-        await f.write(ujson.dumps(json_resp))
     return json_resp["stargazers_count"], json_resp["open_issues_count"]
 
 
@@ -28,9 +23,12 @@ async def main():
     async with aiohttp.ClientSession(headers=HEADERS) as client:
         client: aiohttp.ClientSession
         while True:
-            stars, issues = await fetch_data(client)
-            print(stars, issues)
-            resp = await update_repo_name(client, f"This-Repo-Has-{issues}-Issues-And-Less-Than-{stars+1}-Stars")
-            print(resp)
-            await asyncio.sleep(3)
+            try:
+                stars, issues = await fetch_data(client)
+                print(stars, issues)
+                resp = await update_repo_name(client, f"This-Repo-Has-{issues}-Issues-And-Less-Than-{stars+1}-Stars")
+                print(resp)
+                await asyncio.sleep(3)
+            except:
+                continue
 asyncio.get_event_loop().run_until_complete(main())
